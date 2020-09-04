6f89f8478cfdab6edd8d17d3daed5063d58fcc92
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2020-09-04 20:14:10 +0530

    Change Log- - Added a new Acitivity which is to replace main activity. - Added bottom navigation layout. - Added collapsing toolbar layout. - Updated material dependency with latest version

---
+++ app/src/main/java/com/example/attendo/ui/main/BottomNavMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.attendo.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.attendo.R;
+
+public class BottomNavMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_nav_main);
+    }
+}
