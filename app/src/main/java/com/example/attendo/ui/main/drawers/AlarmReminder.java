de3b8ee0428eec1f04e38fcc79e481aa08d51cd4
your name <you@examole.com>
2020-09-14 00:02:58 +0530

    Alarm duration reduced

--- app/src/main/java/com/example/attendo/ui/main/drawers/AlarmReminder.java
+++ app/src/main/java/com/example/attendo/ui/main/drawers/AlarmReminder.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Build;
+import android.os.Handler;
 import android.provider.Settings;
 
 import androidx.core.app.NotificationCompat;
@@ -18,13 +19,26 @@ import com.example.attendo.ui.auth.AuthenticationActivity;
 
 public class AlarmReminder extends BroadcastReceiver {
 
+    int startfrom=0;
+    int endAt=20000;
+    Runnable stopPlayerTask=new Runnable() {
+        @Override
+        public void run() {
+            mediaPlayer.pause();
+        }
+    };
+
     private static final String CHANNEL_ID="SAMPLE_CHANNEL";
     public MediaPlayer mediaPlayer;
     @Override
     public void onReceive(Context context, Intent intent) {
 
         mediaPlayer=MediaPlayer.create(context, Settings.System.DEFAULT_RINGTONE_URI);
+        mediaPlayer.seekTo(startfrom);
         mediaPlayer.start();
+
+        Handler handler=new Handler();
+        handler.postDelayed(stopPlayerTask,endAt);
 
         //get id and message from intent
         int notificationId = intent.getIntExtra("notificationId",0);
@@ -49,7 +63,7 @@ public class AlarmReminder extends BroadcastReceiver {
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context,CHANNEL_ID)
                 .setSmallIcon(R.drawable.foreground_app_icon)
-                .setContentTitle(" class Reminder!")
+                .setContentTitle("Class Reminder!")
                 .setContentText(message)
                 .setWhen(System.currentTimeMillis())
                 .setAutoCancel(true)
