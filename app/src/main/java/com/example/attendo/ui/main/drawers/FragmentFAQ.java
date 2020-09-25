2a12e2d1383bd1c1c20aeb71b36a9212248782c5
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2020-09-25 12:33:41 +0530

    Change Log- - Faq section is done.

--- app/src/main/java/com/example/attendo/ui/main/drawers/FragmentFAQ.java
+++ app/src/main/java/com/example/attendo/ui/main/drawers/FragmentFAQ.java
@@ -8,10 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.attendo.R;
 
+import me.everything.android.ui.overscroll.OverScrollDecoratorHelper;
+
 public class FragmentFAQ extends Fragment {
+    private LinearLayout faq;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,6 +23,10 @@ public class FragmentFAQ extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_faq, container, false);
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("FAQ");
+
+        faq = view.findViewById(R.id.faq_liner_layout);
+        OverScrollDecoratorHelper.setUpStaticOverScroll(faq, OverScrollDecoratorHelper.ORIENTATION_VERTICAL);
         return view;
     }
+
 }
