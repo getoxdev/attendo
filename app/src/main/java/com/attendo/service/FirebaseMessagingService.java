9ae8d41acab99c68a85168d7a6eb0feb1bd5434b
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2020-10-27 20:46:01 +0530

    Change Log- - Added firebase messaging service to manifest - Added a method to generate token (Tried) - Changed the app icon on splash screen, login and signup.

--- app/src/main/java/com/attendo/service/FirebaseMessagingService.java
+++ app/src/main/java/com/attendo/service/FirebaseMessagingService.java
@@ -1,14 +1,38 @@
 package com.attendo.service;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
+import com.attendo.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService{
 
     @Override
     public void onNewToken(@NonNull String s) {
-        super.onNewToken(s);
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("just", "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                        Log.d("MyToken: ", token);
+
+                    }
+                });
     }
 
     @Override
