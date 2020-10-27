f5f9dad9b90d6891922f3b25bc9f03c7e4d585ec
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2020-10-27 16:59:01 +0530

    Change Log- - Google Sign in feature added

--- app/src/main/java/com/attendo/ui/auth/login/FragmentLogin.java
+++ app/src/main/java/com/attendo/ui/auth/login/FragmentLogin.java
@@ -3,8 +3,11 @@ package com.attendo.ui.auth.login;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.text.TextUtils;
@@ -15,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,22 +27,41 @@ import com.attendo.ui.auth.FragmentForgetPassword;
 import com.attendo.R;
 import com.attendo.ui.auth.signup.FragmentSignup;
 import com.attendo.ui.main.BottomNavMainActivity;
+import com.attendo.ui.main.drawers.account.FragmentProfile;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.transition.MaterialSharedAxis;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.concurrent.Executor;
 
 import static com.airbnb.lottie.L.TAG;
 
 public class FragmentLogin extends Fragment implements logininterface.View {
 
     private EditText email,password;
-    private Button loginbtn;
+    private Button loginbtn, otherWaysbtn;
     private TextView forgotpassword;
     private TextView register;
     private logininterface.Presenter presenter;
     private FragmentSignup fragmentSignup;
     private FragmentForgetPassword fragmentForgetpassword;
     private ProgressBar progress;
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 1234;
+    private FirebaseAuth mAuth;
+    private FragmentProfile fragmentProfile;
 
 
     @Override
@@ -52,11 +75,13 @@ public class FragmentLogin extends Fragment implements logininterface.View {
         forgotpassword = view.findViewById(R.id.textViewforgot);
         register = view.findViewById(R.id.textViewregister);
         progress = view.findViewById(R.id.progress_circular);
+        otherWaysbtn = view.findViewById(R.id.other_signIn_options_btn);
         presenter = new loginPresenter(this);
 
 
         fragmentSignup = new FragmentSignup();
         fragmentForgetpassword = new FragmentForgetPassword();
+        fragmentProfile = new FragmentProfile();
 
         //stay logged in code
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -106,6 +131,39 @@ public class FragmentLogin extends Fragment implements logininterface.View {
 
         ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
 
+        mAuth = FirebaseAuth.getInstance();
+
+        //google sign in option:
+        View otherWaysToSignIn = LayoutInflater.from(getContext()).inflate(R.layout.bottom_sheet_sign_in,
+                (ConstraintLayout) view.findViewById(R.id.sign_in_bottom_sheet));
+        BottomSheetDialog signInBottomSheet = new BottomSheetDialog(getContext(), R.style.BottomSheetDialog);
+        signInBottomSheet.setContentView(otherWaysToSignIn);
+        signInBottomSheet.setDismissWithAnimation(true);
+
+        //create google sign  in request
+        createRequest();
+
+        otherWaysbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInBottomSheet.show();
+                ImageView googleSignInbtn = signInBottomSheet.findViewById(R.id.google_sign_in);
+                ImageView facebookSignInbtn = signInBottomSheet.findViewById(R.id.facebook_sign_in);
+
+                googleSignInbtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //code goes here for google sign in
+                        signIn();
+                        signInBottomSheet.dismiss();
+
+
+                    }
+                });
+            }
+        });
+
+
 
         return view;
     }
@@ -116,6 +174,93 @@ public class FragmentLogin extends Fragment implements logininterface.View {
         password.setEnabled(enable);
         loginbtn.setEnabled(enable);
     }
+
+    //method for google signIn
+    private void createRequest(){
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
+    }
+
+    //google sign in
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener( getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getContext(), "Sign In succesful" ,Toast.LENGTH_SHORT).show();
+
+                            setDataGoogleSignIn(fragmentProfile);
+                            setNextFragment(fragmentProfile);
+                        }
+                        else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Snackbar.make(getView(), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void setNextFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.start_frame, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    private void setDataGoogleSignIn(Fragment fragment) {
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(getContext());
+        String name = null;
+        if(signInAccount!= null){
+            name = signInAccount.getDisplayName();
+        }
+        Bundle bundle = new Bundle();
+        bundle.putString("name", name);
+        bundle.putString("institution","");
+        bundle.putString("city","");
+        bundle.putString("phone","");
+        fragment.setArguments(bundle);
+    }
+
 
     @Override
     public void disableinput() {
