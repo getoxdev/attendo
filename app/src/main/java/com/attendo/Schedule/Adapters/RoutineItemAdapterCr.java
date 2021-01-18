4120b61c51fa511886537255271d3fae49d12085
Shristi Sarma <sarmashristi@hotmail.com>
2021-01-18 15:26:05 +0530

    Bottomsheet added for updating and deleting the schedule.

--- app/src/main/java/com/attendo/Schedule/Adapters/RoutineItemAdapterCr.java
+++ app/src/main/java/com/attendo/Schedule/Adapters/RoutineItemAdapterCr.java
@@ -6,15 +6,21 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.attendo.R;
 import com.attendo.Schedule.Model.SubjectRoutine;
 import com.attendo.data.model.SubjectDetails;
@@ -35,12 +41,14 @@ import butterknife.ButterKnife;
 public class RoutineItemAdapterCr extends RecyclerView.Adapter<RoutineItemAdapterCr.RoutineItemAdapterCrHolder> {
 
     private List<SubjectDetails> items;
+    private Context mContext;
 
 
 
-    public RoutineItemAdapterCr(List<SubjectDetails> items)
+    public RoutineItemAdapterCr(Context mContext,List<SubjectDetails> items)
     {
         this.items = items;
+        this.mContext = mContext;
 
     }
 
@@ -59,8 +67,11 @@ public class RoutineItemAdapterCr extends RecyclerView.Adapter<RoutineItemAdapte
         holder.subject.setText(currentItem.getSubject());
         holder.faculty.setText(currentItem.getFaculty());
         holder.time.setText(currentItem.getTime());
+        String scheduleclassid = currentItem.get_id();
+        Log.e("scheduleclassid",scheduleclassid);
 
 
+        final Vibrator vibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
 
 
 
@@ -68,8 +79,77 @@ public class RoutineItemAdapterCr extends RecyclerView.Adapter<RoutineItemAdapte
         //long pressed....
         holder.mview.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public boolean onLongClick(View v) {
-               return true;
+            public boolean onLongClick(View v) { return true;
+            }
+        });
+
+        holder.subjectCard.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                vibrator.vibrate(100);
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(mContext, R.style.BottomSheetDialog);
+                View bottomsheet = LayoutInflater.from(mContext).inflate(R.layout.bottom_sheet_options_schedule,
+                        (ConstraintLayout) holder.itemView.findViewById(R.id.schedule_bottom_sheet));
+
+                bottomSheetDialog.setContentView(bottomsheet);
+                bottomSheetDialog.setDismissWithAnimation(true);
+                bottomSheetDialog.show();
+
+                TextView editSchedule = bottomsheet.findViewById(R.id.edit_schedule_bottom_sheet);
+                TextView deleteSchedule = bottomsheet.findViewById(R.id.delete_schedule_bottom_sheet);
+
+                bottomSheetDialog.findViewById(editSchedule.getId());
+                bottomSheetDialog.findViewById(deleteSchedule.getId());
+
+
+                deleteSchedule.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        BottomSheetDialog deletebottomsheet = new BottomSheetDialog(mContext, R.style.BottomSheetDialog);
+                        View deletesheet = LayoutInflater.from(mContext).inflate(R.layout.deleteschedule_bottom_sheet,
+                                (ConstraintLayout) holder.itemView.findViewById(R.id.delete_schedule_bottom_sheet));
+
+                        bottomSheetDialog.dismiss();
+
+                        deletebottomsheet.setContentView(deletesheet);
+                        deletebottomsheet.setDismissWithAnimation(true);
+                        deletebottomsheet.show();
+
+                        Button delete = deletebottomsheet.findViewById(R.id.delete_button);
+                        LottieAnimationView deleteanim = deletebottomsheet.findViewById(R.id.lottieAnimationView);
+
+                        deleteanim.setAnimation(R.raw.delete_animation);
+                    }
+                });
+
+                editSchedule.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        BottomSheetDialog bottomSheetDialogedit = new BottomSheetDialog(mContext, R.style.BottomSheetDialog);
+                        View bottomsheet = LayoutInflater.from(mContext).inflate(R.layout.updateschedule_bottomsheet,
+                                (ConstraintLayout) holder.itemView.findViewById(R.id.update_schedule));
+
+                        bottomSheetDialogedit.setContentView(bottomsheet);
+                        bottomSheetDialogedit.setDismissWithAnimation(true);
+                        bottomSheetDialogedit.show();
+
+                        bottomSheetDialog.dismiss();
+
+                        EditText subjectName = bottomSheetDialogedit.findViewById(R.id.edit_subject_bottom_sheet);
+                        Button update = bottomSheetDialogedit.findViewById(R.id.edit_subject_btn);
+                        EditText faculty = bottomSheetDialogedit.findViewById(R.id.update_faculty);
+                        TimePicker timePicker = bottomSheetDialogedit.findViewById(R.id.edit_sub_details_time_picker);
+                        Spinner spinner = bottomSheetDialog.findViewById(R.id.spinner_schedule);
+                        LottieAnimationView animation = bottomSheetDialogedit.findViewById(R.id.lottie_animation_edit_subject_details);
+
+
+
+
+
+                    }
+                });
+                return false;
             }
         });
     }
@@ -88,6 +168,9 @@ public class RoutineItemAdapterCr extends RecyclerView.Adapter<RoutineItemAdapte
         TextView faculty;
         @BindView(R.id.time)
         TextView time;
+
+        @BindView(R.id.subjectCard)
+        CardView subjectCard;
 
         View mview;
 
