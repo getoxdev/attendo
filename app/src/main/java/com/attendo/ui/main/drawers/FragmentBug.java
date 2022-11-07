1fb915b61efcd47d74ffe1ca7a90c4c505f9c729
Rishiraj Paul Chowdhury <rishipc@ip-192-168-29-68.ap-south-1.compute.internal>
2022-11-07 13:42:53 +0530

    updated dependencies

--- app/src/main/java/com/attendo/ui/main/drawers/FragmentBug.java
+++ app/src/main/java/com/attendo/ui/main/drawers/FragmentBug.java
@@ -27,7 +27,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.apache.poi.ss.formula.functions.T;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
