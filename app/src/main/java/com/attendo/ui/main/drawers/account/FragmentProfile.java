087d4fb5109bbe5e7d111aabb49281b658a876f1
navneet098 <imnavneet1234@gmail.com>
2022-11-21 10:40:09 +0530

    FragmentProfile to viewBinding

--- app/src/main/java/com/attendo/ui/main/drawers/account/FragmentProfile.java
+++ app/src/main/java/com/attendo/ui/main/drawers/account/FragmentProfile.java
@@ -23,6 +23,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.attendo.R;
+import com.attendo.databinding.FragmentProfileBinding;
 import com.attendo.ui.main.BottomNavMainActivity;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -42,23 +43,24 @@ import static android.app.Activity.RESULT_OK;
 
 public class FragmentProfile extends Fragment {
 
-    @BindView(R.id.skip_account_creation)
-    TextView SkipAccountCreation;
-    @BindView(R.id.user_name)
-    EditText name;
-    @BindView(R.id.user_school)
-    EditText college;
-    @BindView(R.id.user_city)
-    EditText city;
-    @BindView(R.id.user_phone)
-    EditText Contact;
-    @BindView(R.id.user_image)
-    ImageView profileimage;
-    @BindView(R.id.save_profile)
-    Button btn;
-    @BindView(R.id.user_progressbar)
-    ProgressBar pB;
+//    @BindView(R.id.skip_account_creation)
+//    TextView SkipAccountCreation;
+//    @BindView(R.id.user_name)
+//    EditText name;
+//    @BindView(R.id.user_school)
+//    EditText college;
+//    @BindView(R.id.user_city)
+//    EditText city;
+//    @BindView(R.id.user_phone)
+//    EditText Contact;
+//    @BindView(R.id.user_image)
+//    ImageView profileimage;
+//    @BindView(R.id.save_profile)
+//    Button btn;
+//    @BindView(R.id.user_progressbar)
+//    ProgressBar pB;
 
+    private FragmentProfileBinding binding;
     private FragmentUserProfile fragment_user_profile;
     CardView profilecard;
     private Uri filepath;
@@ -77,7 +79,7 @@ public class FragmentProfile extends Fragment {
 
             try {
                     Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(),filepath);
-                    profileimage.setImageBitmap(bitmap);
+                    binding.userImage.setImageBitmap(bitmap);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -87,25 +89,27 @@ public class FragmentProfile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment__profile, container, false);
-        ButterKnife.bind(this,view);
+        binding=FragmentProfileBinding.inflate(getLayoutInflater(),container,false);
+        View view=binding.getRoot();
+        //View view =  inflater.inflate(R.layout.fragment__profile, container, false);
+       // ButterKnife.bind(this,view);
 
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Create Profile");
 
         fragment_user_profile = new FragmentUserProfile();
         profilecard = view.findViewById(R.id.image_cardview);
-        pB.setVisibility(View.INVISIBLE);
+        binding.userProgressbar.setVisibility(View.INVISIBLE);
 
         //retriving data from previous fragment
         Bundle bundle = this.getArguments();
         String data = bundle.getString("name");
-        name.setText(data);
+        binding.userName.setText(data);
         String data2 = bundle.getString("institution");
-        college.setText(data2);
+        binding.userSchool.setText(data2);
         String data3 = bundle.getString("city");
-        city.setText(data3);
+        binding.userCity.setText(data3);
         String data4 = bundle.getString("phone");
-        Contact.setText(data4);
+        binding.userPhone.setText(data4);
 
 
 
@@ -115,41 +119,42 @@ public class FragmentProfile extends Fragment {
         mAuth = FirebaseAuth.getInstance();
         String user_id = mAuth.getCurrentUser().getUid();
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        binding.saveProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pB.setVisibility(View.VISIBLE);
-                String Name = name.getText().toString();
-                String College =college.getText().toString();
-                String City = city.getText().toString();
-                String contact = Contact.getText().toString().trim();
+                binding.userProgressbar.setVisibility(View.VISIBLE);
+                String Name = binding.userName.getText().toString();
+                String College =binding.userSchool.getText().toString();
+                String City = binding.userCity.getText().toString();
+                String contact = binding.userPhone.getText().toString().trim();
                 if(Name.isEmpty() || College.isEmpty() || City.isEmpty() || contact.isEmpty()) {
                     if(Name.isEmpty() || College.isEmpty() || City.isEmpty() || contact.isEmpty()) {
-                        pB.setVisibility(View.INVISIBLE);
+                        binding.userProgressbar.setVisibility(View.INVISIBLE);
                         Toast.makeText(getContext(), "Please enter all fields", Toast.LENGTH_SHORT).show();
                     }
                  }
                 else {
                     //for generating unique id may not require everytime
                     if (contact.length() != 10) {
-                        pB.setVisibility(View.INVISIBLE);
+                        binding.userProgressbar.setVisibility(View.INVISIBLE);
                         Toast.makeText(getContext(), "Enter a valid Mobile Number of length 10", Toast.LENGTH_SHORT).show();
                     } else {
-                        pB.setVisibility(View.VISIBLE);
+                        binding.userProgressbar.setVisibility(View.VISIBLE);
                         String id = user_id;
-                        ProfileData prf = new ProfileData(id, name.getText().toString().trim(), college.getText().toString(), city.getText().toString().trim(), Contact.getText().toString().trim());
+                        ProfileData prf = new ProfileData(id, binding.userName.getText().toString().trim(), binding.userSchool.getText().toString(),
+                                binding.userCity.getText().toString().trim(), binding.userPhone.getText().toString().trim());
                         databaseReference.child(id).setValue(prf);
 
                         //Profile imgae is optional...
                         if (filepath != null) {
                             UploadImage();
                         } else {
-                            pB.setVisibility(View.INVISIBLE);
+                            binding.userProgressbar.setVisibility(View.INVISIBLE);
                             storageReference.child("images/" + user_id.toString()).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
                                 @Override
                                 public void onSuccess(Void aVoid) {
                                     databaseReference.child("images/" + user_id.toString()).removeValue();
-                                    Toast.makeText(getActivity(), "Account Updated " + name.getText().toString(), Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(getActivity(), "Account Updated " + binding.userName.getText().toString(), Toast.LENGTH_SHORT).show();
                                     Intent intent = new Intent(getActivity(), BottomNavMainActivity.class);
                                     startActivity(intent);
                                     getActivity().finish();
@@ -175,8 +180,8 @@ public class FragmentProfile extends Fragment {
                           .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                               @Override
                               public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                                  pB.setVisibility(View.INVISIBLE);
-                                  Toast.makeText(getActivity(),"Account Updated "+name.getText().toString(),Toast.LENGTH_SHORT).show();
+                                  binding.userProgressbar.setVisibility(View.INVISIBLE);
+                                  Toast.makeText(getActivity(),"Account Updated "+binding.userName.getText().toString(),Toast.LENGTH_SHORT).show();
                                   Intent intent=new Intent(getActivity(), BottomNavMainActivity.class);
                                   startActivity(intent);
                                   getActivity().finish();
@@ -199,7 +204,7 @@ public class FragmentProfile extends Fragment {
 
         //SKIP ACCOUNT CREATION CODE
 
-        SkipAccountCreation.setOnClickListener(new View.OnClickListener() {
+        binding.skipAccountCreation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getContext(),"Your Account Not Updated!",Toast.LENGTH_SHORT).show();
