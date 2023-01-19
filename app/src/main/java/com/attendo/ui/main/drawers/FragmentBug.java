8d114bcc7d223618c4d824787f2575655b1fa7ca
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2023-01-19 12:01:30 +0530

    Fixed the issue of displaying multiple bugs

--- app/src/main/java/com/attendo/ui/main/drawers/FragmentBug.java
+++ app/src/main/java/com/attendo/ui/main/drawers/FragmentBug.java
@@ -1,13 +1,11 @@
 package com.attendo.ui.main.drawers;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +13,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-
 import com.attendo.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -26,6 +24,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Iterator;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -43,7 +42,7 @@ public class FragmentBug extends Fragment{
     FirebaseAuth mAuth;
     ProgressBar Pb;
     int found = 0;
-    String value = "No Data Found";
+    String value = "";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -53,7 +52,21 @@ public class FragmentBug extends Fragment{
         ButterKnife.bind(this,view);
 
         mAuth = FirebaseAuth.getInstance();
-        databaseReference= FirebaseDatabase.getInstance().getReference("Bugs");
+        databaseReference= FirebaseDatabase.getInstance().getReference("Bugs").child(mAuth.getCurrentUser().getUid()).child("Bug");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot child : snapshot.getChildren()) {
+                    found++;
+                    String bugText = child.getValue().toString();
+                    value += "- " + bugText + "\n";
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.i("bugreport" , "Error: " + error);
+            }
+        });
         Pb = view.findViewById(R.id.detail_progress);
 
         send.setOnClickListener(new View.OnClickListener() {
@@ -69,7 +82,7 @@ public class FragmentBug extends Fragment{
                                     Toast.makeText(getActivity(),"Your response send",Toast.LENGTH_SHORT).show();
                                 }
                                 else{
-                                    Toast.makeText(getActivity(),"something went wrong!",Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(getActivity(), R.string.network_error, Toast.LENGTH_SHORT).show();
                                 }
                             }
                         });
@@ -84,31 +97,18 @@ public class FragmentBug extends Fragment{
         details.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Pb.setVisibility(View.VISIBLE);
-                databaseReference.orderByKey().equalTo(mAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if(snapshot.exists()){
-                            Pb.setVisibility(View.INVISIBLE);
-                            found = 1;
-                            value = snapshot.child(mAuth.getCurrentUser().getUid()).child("Bug").getValue().toString();
-                        }
-                        else{
-
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        Toast.makeText(getActivity(),""+error, Toast.LENGTH_SHORT).show();
-                    }
-                });
-                    new AlertDialog.Builder(getContext())
+                if(found <= 0)  {
+                    new MaterialAlertDialogBuilder(requireContext())
                             .setTitle("Sent Details :")
-                            .setMessage(""+value).show();
-                Pb.setVisibility(View.INVISIBLE);
+                            .setMessage("No bugs reported by you")
+                            .show();
+                } else {
+                    new MaterialAlertDialogBuilder(requireContext())
+                            .setTitle("Sent Details :")
+                            .setMessage(value)
+                            .show();
+                }
             }
-
         });
         return view;
     }
