2ff81e90cd9e1e1be1895f785c371007e39eafa9
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2024-02-19 20:27:45 +0530

    Removed dependency on Butterknife

--- app/src/main/java/com/attendo/Schedule/CreateAndJoinClassBottomSheetDialogFragment.java
+++ app/src/main/java/com/attendo/Schedule/CreateAndJoinClassBottomSheetDialogFragment.java
@@ -5,35 +5,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TimePicker;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
 import com.attendo.R;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.transition.MaterialSharedAxis;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
 public class CreateAndJoinClassBottomSheetDialogFragment extends BottomSheetDialogFragment {
 
-    @BindView(R.id.join_class_btn)
     Button joinClass;
-
-    @BindView(R.id.create_class_btn)
     Button createClass;
-
-
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.create_join_class_bottom_sheet, container, false);
-        ButterKnife.bind(this, view);
+
+        // reference to views in old way
+        joinClass = (Button) view.findViewById(R.id.join_class_btn);
+        createClass = (Button) view.findViewById(R.id.create_class_btn);
 
         joinClass.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +36,6 @@ public class CreateAndJoinClassBottomSheetDialogFragment extends BottomSheetDial
 
             }
         });
-
         createClass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,7 +46,6 @@ public class CreateAndJoinClassBottomSheetDialogFragment extends BottomSheetDial
             }
         });
 
-
         return view;
     }
 
@@ -65,6 +55,5 @@ public class CreateAndJoinClassBottomSheetDialogFragment extends BottomSheetDial
         FragmentTransaction fragmentTransaction = getParentFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.container_frame, fragment);
         fragmentTransaction.addToBackStack(null).commit();
-
     }
 }
