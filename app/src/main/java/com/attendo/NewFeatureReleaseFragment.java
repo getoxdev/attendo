b3bc34a1bf9b3cf3fedf19d878f7813436a526b1
JyotimoyKashyap <jyotimoykashyap123@gmail.com>
2024-02-20 23:35:04 +0530

    Replaced butterknife with viewbinding

--- app/src/main/java/com/attendo/NewFeatureReleaseFragment.java
+++ app/src/main/java/com/attendo/NewFeatureReleaseFragment.java
@@ -9,22 +9,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.attendo.databinding.FragmentNewFeatureReleaseBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.transition.MaterialSharedAxis;
 
 public class NewFeatureReleaseFragment extends Fragment
 {
+    private FragmentNewFeatureReleaseBinding binding;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_new_feature_release, container, false);
+        binding = FragmentNewFeatureReleaseBinding.inflate(inflater, container, false);
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("What's New");
         BottomNavigationView bottomNavigationView = getActivity().findViewById(R.id.bottom_nav_bar);
         bottomNavigationView.setVisibility(View.GONE);
         setEnterTransition(new MaterialSharedAxis(MaterialSharedAxis.Z, true));
         setExitTransition(new MaterialSharedAxis(MaterialSharedAxis.Z, false));
-
-        return view;
+        return binding.getRoot();
     }
 }
